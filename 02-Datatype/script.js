13775131f642c8293372a0b9b64a5415e7b238e4
herwan <mdherwan@gmail.com>
2022-01-29 19:20:35 +0800

    data type

---
+++ 02-Datatype/script.js
@@ -0,0 +1,43 @@
+// primitive
+// stored directly in variable
+// stored on the stack (actual values)
+
+// strings
+const name = 'John';
+let hacker = 'Ezzi';
+
+// numbers
+const no = 46;
+
+// boolean
+const hasKids = true;
+
+// null
+const people = null;
+
+// undefined
+let test;
+
+// symbol
+const sym = Symbol();
+
+// Refference type
+// access by refferance
+// store in a heap(dynamic memory)
+
+// arrays
+const car = ['honda', 'toyota', 'nissan'];
+
+// object litterals
+const address = {
+    city: 'Boston',
+    state: 'MA'
+}
+
+// date
+const today = new Date();
+
+// how to see the data type
+console.log(typeof address);
+
+
