78c91e7e580ea1723f8f25c360c7a3aa2beb6100
muhamadherwan <hello.wanlab@gmail.com>
2022-03-25 09:15:11 +0800

    promises

---
+++ async/promises.js
@@ -0,0 +1,30 @@
+// post object array
+const posts = [
+    {title: 'Post one', body: 'This is post one'},
+    {title: 'Post two', body: 'This is post two'}
+];
+
+function createPost(post) {
+    // use promises
+    return new Promise(function(resolve, reject){
+        setTimeout(() => {
+            posts.push(post);
+            resolve();
+        }, 2000);
+    });
+}
+
+function getPosts() {
+    setTimeout(() => {
+        let output = '';
+        posts.forEach((post)=> {
+            output += `<li>${post.title}</li>`;
+        });
+        document.body.innerHTML = output;
+    }, 1000);
+}
+
+// create the new post and callback the getPosts function:
+const newPost = {title: 'Post three', body: 'This is post three'};
+
+createPost(newPost).then(getPosts);
