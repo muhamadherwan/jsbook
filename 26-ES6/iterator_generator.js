26b4114c1356de269e70f4d1493360aa75d3dae3
muhamadherwan <hello.wanlab@gmail.com>
2022-03-31 19:15:13 +0800

    generator

--- 26-ES6/iterator_generator.js
+++ 26-ES6/iterator_generator.js
@@ -16,7 +16,36 @@ const namesArr = ["Ezzi", "Nick", "Donny"];
 // init iterator and pass in the names array
 const names = nameIterator(namesArr);
 
-console.log(names.next().value);
-console.log(names.next().value);
-console.log(names.next().value);
-console.log(names.next().value);
+// console.log(names.next().value);
+// console.log(names.next().value);
+// console.log(names.next().value);
+// console.log(names.next().value);
+
+// Generator example
+function* sayNames() {
+  yield "Jack";
+  yield "Ezzi";
+  yield "Nick";
+}
+
+const name = sayNames();
+
+// console.log(name.next().value);
+// console.log(name.next().value);
+// console.log(name.next().value);
+// console.log(name.next().value);
+
+// id creator
+function* createIds() {
+  let index = 1;
+  while (true) {
+    yield index++;
+  }
+}
+
+const ids = createIds();
+
+console.log(ids.next().value);
+console.log(ids.next().value);
+console.log(ids.next().value);
+console.log(ids.next().value);
