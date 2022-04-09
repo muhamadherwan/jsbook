cf8db5a173bedec3c23ed7052d8cb890a6123270
muhamadherwan <hello.wanlab@gmail.com>
2022-04-09 20:24:27 +0800

    obsever pattern es6

---
+++ 27-Pattern/observer_es6.js
@@ -0,0 +1,55 @@
+// OBSERVER PATTERN ES6
+class EventObserver {
+  constructor() {
+    this.observers = [];
+  }
+
+  subscribe(fn) {
+    this.observers.push(fn);
+    console.log(`You are now subscribed to ${fn.name}`);
+  }
+
+  unsubscribe(fn) {
+    this.observers = this.observers.filter(function (item) {
+      if (item !== fn) {
+        return item;
+      }
+    });
+    console.log(`You are now unsubsribed from ${fn.name}`);
+  }
+
+  fire() {
+    this.observers.forEach(function (item) {
+      item.call();
+    });
+  }
+}
+
+// USE THE PATTERN
+const click = new EventObserver();
+
+// event listeners
+document
+  .querySelector(".sub-ms")
+  .addEventListener("click", () => click.subscribe(getCurMilliseconds));
+
+document
+  .querySelector(".unsub-ms")
+  .addEventListener("click", () => click.unsubscribe(getCurMilliseconds));
+
+document
+  .querySelector(".sub-s")
+  .addEventListener("click", () => click.subscribe(getCurSeconds));
+
+document
+  .querySelector(".unsub-s")
+  .addEventListener("click", () => click.unsubscribe(getCurSeconds));
+
+document.querySelector(".fire").addEventListener("click", () => click.fire());
+
+// click handler
+const getCurMilliseconds = () =>
+  console.log(`current milliseconds: ${new Date().getMilliseconds()}`);
+
+const getCurSeconds = () =>
+  console.log(`current seconds: ${new Date().getSeconds()}`);
